e989c2d64baae6212586c5164a3ccfdf9af9a049
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-07-17 17:54:12 -0500

    feat: add to script.js

---
+++ script/script.js
@@ -0,0 +1,27 @@
+const keys = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+
+function getRandomNumber(min,max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random()*(max-min+1))+min;  //The maximum is inclusive and the minimum is inclusive
+}
+
+function getRandomKey() {
+  return keys[getRandomNumber(0,keys.length-1)];
+}
+
+function getRandomKey() {
+  const key = document.getElementById(getRandomKey());
+  key.classList.add("selected");
+}
+
+document.addEventListener("keyup", event => {
+  const keyPressed = String.fromCharCode(event.keyCode);  //key
+  const keyElement = document.getElementById(keyPressed);
+  const highLigthKey = document.querySelector(".selected");
+  keyElement.classList.add("hit");
+  keyElement.addEventListener('animationend', () => {
+    keyElement.classList.remove("hit")
+    })
+  } 
+)
